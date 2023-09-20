c328e51f0d5fb686e5b3aeeb3301d546af17c8e1
Juan Pablo Canseco Rios <juan.canseco.rios@gmail.com>
2023-09-20 09:43:51 -0600

    add eureka-server

---
+++ services/eureka-server/src/test/java/com/jcanseco/dev/bank/minibank/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcanseco.dev.bank.minibank.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,8 @@ rootProject.name = "minibank-microservices"
 include("common")
 include("services:cards-service")
 include("services:loans-service")
+include("services:eureka-server")
 findProject(":services:cards-service")?.name = "cards-service"
 findProject(":services:loans-service")?.name = "loans-service"
-
+findProject(":services:eureka-server")?.name = "eureka-server"
 
