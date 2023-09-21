61c5bbcac76d963e0eeff6cdfbc72a8068ad64f8
Juan Pablo Canseco Rios <juan.canseco.rios@gmail.com>
2023-09-21 17:22:45 -0600

    gateway working

--- services/gateway-server/src/main/java/com/jcanseco/dev/bank/minibank/gatewayserver/GatewayServerApplication.java
+++ services/gateway-server/src/main/java/com/jcanseco/dev/bank/minibank/gatewayserver/GatewayServerApplication.java
@@ -2,12 +2,37 @@ package com.jcanseco.dev.bank.minibank.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import java.util.Date;
 
 @SpringBootApplication
 public class GatewayServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServerApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator buildRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p
+						.path("/api/accounts/**")
+						.filters(f -> f.rewritePath("/api/accounts/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time",new Date().toString()))
+						.uri("lb://ACCOUNTS-SERVICE")).
+				route(p -> p
+						.path("/api/loans/**")
+						.filters(f -> f.rewritePath("/api/loans/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time",new Date().toString()))
+						.uri("lb://LOANS-SERVICE")).
+				route(p -> p
+						.path("/api/cards/**")
+						.filters(f -> f.rewritePath("/api/cards/(?<segment>.*)","/${segment}")
+								.addResponseHeader("X-Response-Time",new Date().toString()))
+						.uri("lb://CARDS-SERVICE"))
+				.build();
 	}
 
 }
